3e5c4bf5b02596de96dde181117238a172ca406f
paulofreitas <pauloroberto.lds@gmail.com>
2025-11-06 19:42:40 -0400

    subset and sunab() finished

--- dissertacao1.R
+++ dissertacao1.R
@@ -80,7 +80,7 @@ iplot(res_sa20)
 
 ######## 2008 a 2022 ##########
 
-
+rm(list=ls())
 # describe the df$ano_observacao variable
 summary(df$ano_observacao)
 #remove observations with ano_observacao < 2008 and > 2022 
@@ -91,6 +91,7 @@ df <- df %>%
 
 df_1 = df %>%
   filter(ano_observacao >= 2008 & ano_observacao <= 2022)
+summary(df_1$ano_observacao)
 
 # First, create the 'event_time' variable as before
 df_1 <- df_1 %>%
@@ -141,10 +142,13 @@ summary(res_sa20)
 
 iplot(res_sa20)
 
-summary(df_1$ano_observacao)
 
-######## 2009 a 2021 ##########
+res_sa20 = feols( log(remuneracao_media_real)~ sunab(ano_conclusao, event_time) | cpf + ano_observacao, data=df_1)
+summary(res_sa20)
+iplot(res_sa20)
 
+######## 2009 a 2022 ##########
+rm(list=ls())
 
 # describe the df$ano_observacao variable
 summary(df$ano_observacao)
@@ -155,22 +159,22 @@ df <- df %>%
 #"desistente","desligado"
 
 df_1 = df %>%
-  filter(ano_observacao >= 2009 & ano_observacao <= 2021)
+  filter(ano_observacao >= 2009 & ano_observacao <= 2022)
 
 # First, create the 'event_time' variable as before
 df_1 <- df_1 %>%
   group_by(cpf) %>%
   mutate(
-    event_time = if_else(!is.na(ano_conclusao), ano_observacao - ano_conclusao, NA_real_)
-  ) %>%
-  # Bin endpoints for a cleaner plot
-  mutate(
-    event_time = case_when(
-      event_time < -5 ~ -5,
-      event_time > 5  ~ 5,
-      TRUE ~ event_time
-    )
-  )
+    event_time = if_else(!is.na(ano_conclusao), ano_observacao - ano_conclusao, NA_real_))
+  # ) %>%
+  # # Bin endpoints for a cleaner plot
+  # mutate(
+  #   event_time = case_when(
+  #     event_time < -5 ~ -5,
+  #     event_time > 5  ~ 5,
+  #     TRUE ~ event_time
+  #   )
+  # )
 
 # Bin endpoints for a cleaner plot
 df_1 <- df_1 %>%
@@ -201,18 +205,25 @@ data("base_stagg")
 head(base_stagg)
 res_sa20 = feols(y ~ x1 + sunab(year_treated, year) | id + year, base_stagg)
 
+
 res_sa20 = feols(log(remuneracao_media) ~ sunab(ano_conclusao, ano_observacao) | cpf + ano_observacao, data=df_1)
 summary(res_sa20)
 
 iplot(res_sa20)
 
-summary(df_1$ano_observacao)
 
-######## 2010 a 2020 ##########
+res_sa20 = feols(log(remuneracao_media_real) ~ sunab(ano_conclusao, event_time) | cpf + ano_observacao, data=df_1)
+summary(res_sa20)
+
+iplot(res_sa20)
+
+
+
+######## 2010 a 2022 ##########
 rm(list=ls())
 
 # describe the df$ano_observacao variable
-summary(df$ano_observacao)
+
 #remove observations with ano_observacao < 2010 and > 2020 
 df = read_xlsx("/home/freitas/Downloads/0research/nilson/outubro/5. dados_longos_grupos.xlsx")
 df <- df %>%
@@ -220,22 +231,22 @@ df <- df %>%
 #"desistente","desligado"
 
 df_1 = df %>%
-  filter(ano_observacao >= 2010 & ano_observacao <= 2020)
-
+  filter(ano_observacao >= 2010 & ano_observacao <= 2022)
+summary(df_1$ano_observacao)
 # First, create the 'event_time' variable as before
 df_1 <- df_1 %>%
   group_by(cpf) %>%
   mutate(
-    event_time = if_else(!is.na(ano_conclusao), ano_observacao - ano_conclusao, NA_real_)
-  ) %>%
-  # Bin endpoints for a cleaner plot
-  mutate(
-    event_time = case_when(
-      event_time < -5 ~ -5,
-      event_time > 5  ~ 5,
-      TRUE ~ event_time
-    )
-  )
+    event_time = if_else(!is.na(ano_conclusao), ano_observacao - ano_conclusao, NA_real_))
+  # ) %>%
+  # # Bin endpoints for a cleaner plot
+  # mutate(
+  #   event_time = case_when(
+  #     event_time < -5 ~ -5,
+  #     event_time > 5  ~ 5,
+  #     TRUE ~ event_time
+  #   )
+  # )
 
 # Bin endpoints for a cleaner plot
 df_1 <- df_1 %>%
@@ -266,10 +277,158 @@ data("base_stagg")
 head(base_stagg)
 res_sa20 = feols(y ~ x1 + sunab(year_treated, year) | id + year, base_stagg)
 
+
 res_sa20 = feols(log(remuneracao_media) ~ sunab(ano_conclusao, ano_observacao) | cpf + ano_observacao, data=df_1)
 summary(res_sa20)
 
 iplot(res_sa20)
 
+
+res_sa20 = feols(log(remuneracao_media_real) ~ sunab(ano_conclusao, event_time) | cpf + ano_observacao, data=df_1)
+summary(res_sa20)
+
+iplot(res_sa20)
+
+######## 2011 a 2021 ##########
+rm(list=ls())
+
+# describe the df$ano_observacao variable
+
+#remove observations with ano_observacao < 2010 and > 2020 
+df = read_xlsx("/home/freitas/Downloads/0research/nilson/outubro/5. dados_longos_grupos.xlsx")
+df <- df %>%
+  filter(!situação %in% c("matriculado"))
+#"desistente","desligado"
+
+df_1 = df %>%
+  filter(ano_observacao >= 2011 & ano_observacao <= 2022)
 summary(df_1$ano_observacao)
+# First, create the 'event_time' variable as before
+df_1 <- df_1 %>%
+  group_by(cpf) %>%
+  mutate(
+    event_time = if_else(!is.na(ano_conclusao), ano_observacao - ano_conclusao, NA_real_))
+# ) %>%
+# # Bin endpoints for a cleaner plot
+# mutate(
+#   event_time = case_when(
+#     event_time < -5 ~ -5,
+#     event_time > 5  ~ 5,
+#     TRUE ~ event_time
+#   )
+# )
+
+# Bin endpoints for a cleaner plot
+df_1 <- df_1 %>%
+  group_by(cpf) %>%
+  mutate(treated_group = max(mestre, na.rm = TRUE)) %>%
+  ungroup()
+
+# create time_to_treatement variable which is equal to event_time for treated units and -1000 for never treated
+df_1 <- df_1 %>%
+  mutate(time_to_treatment = if_else(treated_group == 1, event_time, -1000))
+
+
+# TWFE DiD (note that the time to treatment for the never treated is -1000)
+# (by using ref = c(-1, -1000) we exclude the period just before the treatment and 
+# the never treated)
+res_twfe = feols(log(remuneracao_media) ~ i(time_to_treatment, ref = c(-1, -1000)) | cpf + ano_observacao, data=df_1)
+res_twfe_real = feols(log(remuneracao_media_real) ~ i(time_to_treatment, ref = c(-1, -1000)) | cpf + ano_observacao, data=df_1)
+summary(res_twfe_real)
+summary(res_twfe)
+
+# Compare the results
+# Plot the two TWFE results
+iplot(res_twfe)
+iplot(res_twfe_real)
+
+
+data("base_stagg")
+head(base_stagg)
+res_sa20 = feols(y ~ x1 + sunab(year_treated, year) | id + year, base_stagg)
+
+
+res_sa20 = feols(log(remuneracao_media) ~ sunab(ano_conclusao, ano_observacao) | cpf + ano_observacao, data=df_1)
+summary(res_sa20)
+
+iplot(res_sa20)
+
+
+res_sa20 = feols(log(remuneracao_media_real) ~ sunab(ano_conclusao, event_time) | cpf + ano_observacao, data=df_1)
+summary(res_sa20)
+
+iplot(res_sa20)
+
+
+######## 2012 a 2022 ##########
+rm(list=ls())
+
+# describe the df$ano_observacao variable
+
+#remove observations with ano_observacao < 2010 and > 2020 
+df = read_xlsx("/home/freitas/Downloads/0research/nilson/outubro/5. dados_longos_grupos.xlsx")
+df <- df %>%
+  filter(!situação %in% c("matriculado"))
+#"desistente","desligado"
+
+df_1 = df %>%
+  filter(ano_observacao >= 2012 & ano_observacao <= 2022)
+summary(df_1$ano_observacao)
+# First, create the 'event_time' variable as before
+df_1 <- df_1 %>%
+  group_by(cpf) %>%
+  mutate(
+    event_time = if_else(!is.na(ano_conclusao), ano_observacao - ano_conclusao, NA_real_))
+# ) %>%
+# # Bin endpoints for a cleaner plot
+# mutate(
+#   event_time = case_when(
+#     event_time < -5 ~ -5,
+#     event_time > 5  ~ 5,
+#     TRUE ~ event_time
+#   )
+# )
+
+# Bin endpoints for a cleaner plot
+df_1 <- df_1 %>%
+  group_by(cpf) %>%
+  mutate(treated_group = max(mestre, na.rm = TRUE)) %>%
+  ungroup()
+
+# create time_to_treatement variable which is equal to event_time for treated units and -1000 for never treated
+df_1 <- df_1 %>%
+  mutate(time_to_treatment = if_else(treated_group == 1, event_time, -1000))
+
+
+# TWFE DiD (note that the time to treatment for the never treated is -1000)
+# (by using ref = c(-1, -1000) we exclude the period just before the treatment and 
+# the never treated)
+res_twfe = feols(log(remuneracao_media) ~ i(time_to_treatment, ref = c(-1, -1000)) | cpf + ano_observacao, data=df_1)
+res_twfe_real = feols(log(remuneracao_media_real) ~ i(time_to_treatment, ref = c(-1, -1000)) | cpf + ano_observacao, data=df_1)
+summary(res_twfe_real)
+summary(res_twfe)
+
+# Compare the results
+# Plot the two TWFE results
+iplot(res_twfe)
+iplot(res_twfe_real)
+
+
+data("base_stagg")
+head(base_stagg)
+res_sa20 = feols(y ~ x1 + sunab(year_treated, year) | id + year, base_stagg)
+
+
+res_sa20 = feols(log(remuneracao_media) ~ sunab(ano_conclusao, ano_observacao) | cpf + ano_observacao, data=df_1)
+summary(res_sa20)
+
+iplot(res_sa20)
+
+
+res_sa20 = feols(log(remuneracao_media_real) ~ sunab(ano_conclusao, event_time) | cpf + ano_observacao, data=df_1)
+summary(res_sa20)
+
+iplot(res_sa20)
+
+
 
